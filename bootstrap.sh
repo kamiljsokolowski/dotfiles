8cfe3667fcb77e95f30e9c028be2837976928df5
sokolowskik <kamil.j.sokolowski@gmail.com>
2015-08-10 23:42:07 +0200

    define repo location (or use current dir)

---
+++ bootstrap.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+dir=$1
+#basedir="cd "$(dirname "${BASH_SOURCE[0]}")" && pwd"
+basedir=`pwd`
+
+if [ "$dir" = "" ]; then
+	echo "Repo dir (hit ENTER to use '$basedir')"
+	read dir
+fi
+
+if [ "$dir" = "" ]; then
+	dir=$basedir
+fi
+
