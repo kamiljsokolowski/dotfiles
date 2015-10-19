19c5e7283642189fbddc50b6ee7e478fb178bd54
Kamil Sokolowski <kamil.j.sokolowski@gmail.com>
2015-10-19 14:57:07 +0200

    (if it exists) remove default ~/.vim directory

--- bootstrap.sh
+++ bootstrap.sh
@@ -29,6 +29,9 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 if [ -f ~/.vimrc ]; then rm -rf ~/.vimrc
 fi
 ln -s ${basedir}/vimrc ~/.vimrc
+if [ -d ~/.vim ]; then rm -rf ~/.vim
+fi
+ln -s ${basedir}/vim ~/.vim
 # Powerline (requires pip)
 command -v pip >/dev/null 2>&1 || { echo >&2 "Did not found pip.. no powerlining for You today mate!"; exit 1; }
 pip install --user powerline-status
